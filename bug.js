523745c3d4a3e381f76711d544e90f8f7a782ede
BugHunter-X <askzenai@gmail.com>
2024-12-31 10:52:05 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,1 @@
+The Firebase Realtime Database sometimes throws an error, "Data has already been updated," even when no other client is modifying the data. This typically occurs when you perform multiple writes to the same location in quick succession.
